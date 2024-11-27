2ee93bbbf2452ee6f3f2cc5920e0efa334e31340
Dmytro Kulish <mitiakylish2@gmail.com>
2024-11-27 23:36:37 +0200

    Added minAndMax method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3};
         //Task 1
         System.out.println("Середня значення елементів масиву = "+ averageNumber(arr));
+        //Task 2
+        minAndMax(arr);
     }
     public static int averageNumber(int[] numbers) {
         int sum = 0;
@@ -11,5 +15,12 @@ public class Main {
         }
         return sum / numbers.length;
 
+    }
+    public static void minAndMax(int[] numbers) {
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        int max = numbers[numbers.length - 1];
+        int min = numbers[0];
+        System.out.println("min = " + min + " max = " + max);
     }
 }
