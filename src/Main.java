7121a0b3764d5a36564ea021df4567b9f768a67f
Dmytro Kulish <mitiakylish2@gmail.com>
2024-12-08 16:53:13 +0200

    added countEvenInts method

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Task 8.1
-                int[] array = {1,3,5};
-                System.out.println(countEvenInts(array));
+        int[] array = {1,3,5};
+        System.out.println(countEvenInts(array));
+    }
+    public static int countEvenInts(int [] array) {
+        int count = 0;
+        for(int i: array){
+            if(i%2==0){
+                count++;
             }
-            public static int countEvenInts(int [] array) {
-                int count = 0;
-                for(int i: array){
-                    if(i%2==0){
-                        count++;
-                    }
-                }
-                return count;
+        }
+        return count;
     }
 }
