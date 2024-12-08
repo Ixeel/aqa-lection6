6966168b813027fea50bf737aa129119ea299b59
Dmytro Kulish <mitiakylish2@gmail.com>
2024-12-08 21:50:04 +0200

    added centeredAverage method

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,3,5};
+        //Task 8.1
+        int[] array = {2,2,0};
         System.out.println(countEvenInts(array));
+
+        //Task 8.2
+        int[] array2 = {-10, -4, -2, -4, -2, 0};
+        System.out.println(centeredAverage(array2));
     }
     public static int countEvenInts(int [] array) {
         int count = 0;
@@ -13,5 +18,24 @@ public class Main {
             }
         }
         return count;
+    }
+    public static int centeredAverage(int[] arr) {
+        int min = 0;
+        int max = 0;
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        sum = sum - min;
+        sum = sum - max;
+
+        return sum / (arr.length - 2);
     }
 }
